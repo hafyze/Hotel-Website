daeeb799843e5f0cbd13a645a3e7f79f335db9cc
hafyze <84822836+hafyze@users.noreply.github.com>
2023-01-17 23:01:13 +0800

    Add files via upload

--- Hotel Website/php/add_room.php
+++ Hotel Website/php/add_room.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="css/admin.css">
-    <link rel="stylesheet" href="css/mobile.css">
+    <link rel="stylesheet" type="text/css" href="../css/admin.css">
+    <link rel="stylesheet" type="text/css" href="../css/mobile.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -21,7 +21,7 @@
     </header>
 
     <div class="sidebar">
-        <a href="admin.html"><i class="fa fa-fw fa-home"></i> Admin</a>
+        <a href="../admin.html"><i class="fa fa-fw fa-home"></i> Admin</a>
         <a href="#"><i class="fa-solid fa-bed"></i> Services</a>
         <a href="#"><i class="fa fa-fw fa-user"></i> Staffs</a>
         <a href="#"><i class="fa-solid fa-user-tie"></i> Member</a>
@@ -35,13 +35,24 @@
             <div class="service">
                 <h2>Insert New Service</h2>
 
-                <form class="addroom" name="addfrm" method="post" action="php/add_room.php">
-                    <p><label for="room_name">Service: </label>
+                <form class="addroom" name="addfrm" method="post" action="php/add_movie.php">
+                    <p>
+                        <label for="room_name">Service: </label>
                         <input type="text" name="room_name" size="80"></p>
-                    <p><label for="room_price">Room Price:</label>
+                    <p>
+                        <label for="room_price">Room Price:</label>
                         <input type="text" name="room_price" size="10"></p>
                     <p>
                         <input type="submit" name="savebtn" value="Save Room">
+                    </p>
+                    <p>
+                        <a class="button_feature" href="view_room.php">View Lists</a>
+                    </p>
+                    <p>
+                        <a class="button_feature" href="delete_room.php">Delete Room</a>
+                    </p>
+                    <p>
+                        <a class="button_feature" href="update_room.php">Update Room</a>
                     </p>
                 </form>
             </div>
@@ -50,3 +61,27 @@
 </body>
 
 </html>
+
+<?php
+    $connection = new mysqli("localhost", "root", "", "hotel");
+    //echo "Connection successful";
+
+    $roomName = $_POST["room_name"];
+    $roomPrice = $_POST["room_price"];
+
+    if($connection->connect_error){
+        die("Connection failed: " . $connection->connect_error);
+    }else{
+        $sql = $connection->prepare("INSERT INTO room(room_name, room_price)
+                VALUES(?, ?)");
+        $sql->bind_param("si", $roomName, $roomPrice);
+        $sql->execute();
+        //echo "Registration successul";
+        $sql->close();
+        $connection->close();
+    }
+
+    
+
+    
+?>
